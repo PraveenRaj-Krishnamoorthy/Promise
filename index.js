ca7f2e0ebc6592fca74278527cca4ce5f4d68109
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-03-26 21:37:54 +0530

    Promise resolve&reject
    Promise method 1 completed.

---
+++ index.js
@@ -0,0 +1,34 @@
+// Method One
+// function func(params) {
+//     return new Promise((resolve,reject)=>{
+//         let on = true
+//         if (on) {
+//             resolve()
+//         } else {
+//             reject()
+//         }
+//     })
+// }
+
+// func().then(()=>{
+//     console.log("Resolved");
+// }).catch(()=>{
+//     console.log("Not Resolved!");
+// })
+// -----------------------------------------------//
+//Method Two!
+
+function upload(params) {
+    console.log("Uploading!");
+    let isupload = true
+    setTimeout(()=>{
+        
+    })
+}
+
+function publish(params) {
+    console.log("Publishing!");
+    setTimeout(() => {
+
+    })
+}
